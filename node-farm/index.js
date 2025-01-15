6a750937fd9b1647c7ebf9b6e00490ebd22a5be7
therafiniac <therafiniac@gmail.com>
2025-01-15 18:15:10 +0530

    add slugify and gitignore to node-farm

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -17,6 +19,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slug = dataObj.map((el) => slugify(el.productName, { lowe: true }));
+console.log(slug);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
