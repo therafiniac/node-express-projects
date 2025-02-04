b6e558121c8680bf6fb644dd9ed5c9fa49e28311
therafiniac <therafiniac@gmail.com>
2025-02-04 18:31:15 +0530

    Add pagination in natours project

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -29,6 +29,17 @@ exports.getAllTour = async (req, res) => {
       query = query.select('-__V');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 1;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page doesn't exist");
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
